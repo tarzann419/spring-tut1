37a0cfdc1be2b156a49f9a939ef2bc48f0c6678a
dantheman <danogbo0@gmail.com>
2023-10-21 23:23:56 +0100

    recent commit

--- java/spring-amigos/src/main/java/com/example/demo/student/Student.java
+++ java/spring-amigos/src/main/java/com/example/demo/student/Student.java
@@ -1,16 +1,30 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+@Entity //for hibernate
+@Table // for table in db
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence  "
+    )
     private Long id;
     private String name;
     private Integer age;
     private LocalDate dob;
     private String email;
 
-
     public Student(Long id, String name, Integer age, LocalDate dob, String email) {
+
         this.id = id;
         this.name = name;
         this.age = age;
